e52b204aee15d33b1ec9809b32f30aad29f091a9
Marvin Quevedo <marvinquevedo2017@outlook.es>
2023-03-08 00:42:14 -0600

    remove py3 to compile in Flutter

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,9 @@ extern crate indoc;
 #[macro_use]
 extern crate do_notation;
 
-#[macro_use]
-#[cfg(all(not(test), not(target_family = "wasm"), feature = "extension-module"))]
-extern crate pyo3;
+// #[macro_use]
+// #[cfg(all(not(test), not(target_family = "wasm"), feature = "extension-module"))]
+// extern crate pyo3;
 
 extern crate tempfile;
 
@@ -23,9 +23,9 @@ pub mod util;
 pub mod classic;
 pub mod compiler;
 
-// Python impl
-#[cfg(all(not(test), not(target_family = "wasm"), feature = "extension-module"))]
-mod py;
+// // Python impl
+// #[cfg(all(not(test), not(target_family = "wasm"), feature = "extension-module"))]
+// mod py;
 
 #[cfg(test)]
 mod tests;
